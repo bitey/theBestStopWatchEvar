d12dd64915a7e76e696dc960093317ccbe075ace
bitey <bitey@outlook.com>
2013-02-10 18:55:02 -0600

    Initial Commit

---
+++ theBestStopWatchEvar/MMAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MMAppDelegate.h
+//  theBestStopWatchEvar
+//
+//  Created by StopBitingMe on 2/10/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MMViewController;
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MMViewController *viewController;
+
+@end
---
+++ theBestStopWatchEvar/MMViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMViewController.h
+//  theBestStopWatchEvar
+//
+//  Created by StopBitingMe on 2/10/13.
+//  Copyright (c) 2013 StopBitingMe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMViewController : UIViewController
+
+@end
